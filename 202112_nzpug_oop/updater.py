ae0a4a41ed422df0440de73b2f01545b1379598b
Rodrigo Girão Serrão <rodrigogiraoserrao@gmail.com>
2022-01-19 22:37:27 +0000

    Add updater/helper file.

---
+++ 202112_nzpug_oop/updater.py
@@ -0,0 +1,19 @@
+from time import sleep
+
+from git import Repo
+
+PATH = "../.git"
+COMMIT = "Auto push."
+
+def push():
+    repo = Repo(PATH)
+    repo.git.add(update=True)
+    repo.index.commit(COMMIT)
+    origin = repo.remote(name="origin")
+    origin.push()
+
+
+if __name__ == "__main__":
+    while True:
+        push()
+        sleep(60)
