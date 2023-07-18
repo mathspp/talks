5f2880219e358c83b003a5ec2cc7b5dcc5f4faab
Rodrigo Girão Serrão <5621605+rodrigogiraoserrao@users.noreply.github.com>
2023-07-18 16:50:35 +0200

    Auto push.

--- 20230718-practical_introduction_to_descriptors/full_person.py
+++ 20230718-practical_introduction_to_descriptors/full_person.py
@@ -1,7 +1,14 @@
 class NameDescriptor:
-    def __init__
+    def __init__(self, idx):
+        self.idx
+
+    def __get__(self, person, cls):
+        return person.name.split()[self.idx]
 
 class FullNamePerson:
     first = NameDescriptor(0)
     middle = NameDescriptor(slice(1, -1))
     last = NameDescriptor(-1)
+
+    def __init__(self, name):
+        self.name = name
