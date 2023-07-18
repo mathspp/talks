ab1bed543c10467861f3c04e75f481acbc7a9c34
Rodrigo Girão Serrão <5621605+rodrigogiraoserrao@users.noreply.github.com>
2023-07-18 16:13:44 +0200

    Auto push.

--- 20230718-practical_introduction_to_descriptors/person_descriptor.py
+++ 20230718-practical_introduction_to_descriptors/person_descriptor.py
@@ -1,16 +1,25 @@
+import datetime as dt
+
 class NameDescriptor:
     def __get__(self, person, cls):
         print(person, cls)
         return f"{person.first} {person.last}"
 
+
+class AgeDescriptor:
+    def __get__(self, person, cls):
+        return dt.date.today().year - person.birthyear
+
 class Person:
+    age = AgeDescriptor()
     name = NameDescriptor()
 
-    def __init__(self, first, last):
+    def __init__(self, first, last, birthyear):
         self.first = first
         self.last = last
+        self.birthyear = birthyear
 
 
-john = Person("John", "Smith")
+john = Person("John", "Smith", 1997)
 print(john.name)
-print(Person.name)
+print(john.age)
