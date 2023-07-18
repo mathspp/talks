d4163cbded7dfc4a0675eabebca75ad29ede82a3
Rodrigo Girão Serrão <5621605+rodrigogiraoserrao@users.noreply.github.com>
2023-07-18 16:40:20 +0200

    Auto push.

---
+++ 20230718-practical_introduction_to_descriptors/person_set.py
@@ -0,0 +1,16 @@
+class NameDescriptor:
+    def __get__(self, person, cls):
+        print(person, cls)
+        return f"{person.first} {person.last}"
+
+
+class Person:
+    name = NameDescriptor()
+
+    def __init__(self, first, last):
+        self.first = first
+        self.last = last
+
+
+john = Person("John", "Smith")
+print(john.name)
