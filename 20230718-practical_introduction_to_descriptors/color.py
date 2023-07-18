23120c295f76a07277cd50aa4744564f59f90856
Rodrigo Girão Serrão <5621605+rodrigogiraoserrao@users.noreply.github.com>
2023-07-18 16:24:58 +0200

    Auto push.

--- 20230718-practical_introduction_to_descriptors/color.py
+++ 20230718-practical_introduction_to_descriptors/color.py
@@ -1,19 +1,19 @@
 class RGBComponent:
-    def __init__(self, ...):
-        ...
+    def __init__(self, idx):
+        self.idx = idx
 
     def __get__(self, ...):
         ...
 
 
 class Color:
-    r = RGBComponent(...)
-    g = RGBComponent(...)
-    b = RGBComponent(...)
+    r = RGBComponent(0)
+    g = RGBComponent(1)
+    b = RGBComponent(2)
     hello = "Hello"
 
     def __init__(self, hex):
         self.hex = hex
 
-clr = Color("#ff0000")
+clr = Color("#ff0000")  # ~ (ff, 00, 00)
 print(clr.hello)
