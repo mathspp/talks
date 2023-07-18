11c4f2a859cafba2eb855821903c989e4bc7bed3
Rodrigo Girão Serrão <5621605+rodrigogiraoserrao@users.noreply.github.com>
2023-07-18 16:55:43 +0200

    Auto push.

--- 20230718-practical_introduction_to_descriptors/funcs.py
+++ 20230718-practical_introduction_to_descriptors/funcs.py
@@ -1,2 +1,14 @@
 def get_name(self):
     return self.first + " " + self.last
+
+
+class Person:
+    def __init__(self, first, last):
+        self.first = first
+        self.last = last
+
+    get_name = get_name
+
+
+john = Person("John", "Smith")
+print(john.get_name())
